b6f53f5304cc4aa7f4bfa6a126298a934b2892a5
Luis Rovirosa <luisrovirosa@gmail.com>
2018-03-12 15:20:35 +0100

    test with mockito

--- src/test/java/PrintDateTest.java
+++ src/test/java/PrintDateTest.java
@@ -4,6 +4,9 @@ import java.util.Date;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class PrintDateTest {
     @Test
@@ -16,5 +19,18 @@ public class PrintDateTest {
         printDate.printCurrentDate();
 
         assertThat(printer.hasBeenCalledWith(expectedDate.toString()), is(true));
+    }
+
+    @Test
+    public void should_print_the_current_date_with_mockito() {
+        Printer printer = mock(Printer.class);
+        Calendar calendar = mock(Calendar.class);
+        Date expectedDate = new Date();
+        when(calendar.now()).thenReturn(expectedDate);
+        PrintDate printDate = new PrintDate(printer, calendar);
+
+        printDate.printCurrentDate();
+
+        verify(printer).print(expectedDate.toString());
     }
 }
