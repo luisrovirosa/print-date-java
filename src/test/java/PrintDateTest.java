c17008b09c3cb7f7467df557edc3077d0b0f79c2
Luis Rovirosa <luisrovirosa@gmail.com>
2018-03-12 14:58:12 +0100

    Kata preparation

---
+++ src/test/java/PrintDateTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class PrintDateTest {
+    @Test
+    public void printDate() {
+        PrintDate printDate = new PrintDate();
+
+        printDate.printCurrentDate();
+
+        // How can we test this function?
+    }
+}
